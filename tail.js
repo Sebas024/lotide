d0fcb190b0dff2fef5a1452a77aa9b8bf7aea9d1
Sebas024 <charles.deo024@hotmail.ca>
2023-05-21 02:52:54 -0600

    me

---
+++ tail.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const tail = function(array) {
+  if (array.length <= 1) {
+    return [];
+  }
+  return array.slice(1);
+};
+
+
+assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString()); 
+assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).toString(), ["Lighthouse", "Labs"].toString());
+
+
+assertEqual(tail([5]).toString(), [].toString()); 
+assertEqual(tail([]).toString(), [].toString()); 
+
+
