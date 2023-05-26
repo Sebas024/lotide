6b738306c89a71b0a2311f8f453525ef9eb5e6a0
Charles Samonte <charles.deo024@hotmail.ca>
2023-05-26 15:33:08 -0600

    me

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -10,4 +18,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-module.exports = eqArrays;
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
---
+++ middle.js
@@ -0,0 +1,28 @@
+const middle = function(array) {
+  const length = array.length;
+  const middleIndex = Math.floor(length / 2);
+
+  if (length <= 2) {
+    return [];
+  } else if (length % 2 === 1) {
+    return [array[middleIndex]];
+  } else {
+    return [array[middleIndex - 1], array[middleIndex]];
+  }
+};
+
+console.log(middle([5, 10, 90]));
+
+// // TEST CODE
+// console.log("Middle of [1]:");
+// assertArraysEqual(middle([1]), []);
+// console.log("Middle of [1, 2]:");
+// assertArraysEqual(middle([1, 2]), []);
+// console.log("Middle of [1, 2, 3]:");
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// console.log("Middle of [1, 2, 3, 4, 5]:");
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// console.log("Middle of [1, 2, 3, 4]:");
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// console.log("Middle of [1, 2, 3, 4, 5, 6]:");
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
