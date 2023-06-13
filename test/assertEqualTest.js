ebba86f3b9d2000959b22a2f6be7492102055186
Charles Samonte <charles.deo024@hotmail.ca>
2023-06-12 22:27:45 -0600

    updates

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-=======
 const assertEqual = require('../assertEqual');
 
->>>>>>> 6f1dcdca022e8c570b3adee4c61d27a819e25829
